775cce822fa58b6ba15e9dae7ede1bc7a1e69fe0
asipheJ <asiphehashe@yahoo.com>
2024-11-18 14:50:10 +0200

    Add files via upload

---
+++ src/components/Credit.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Credit() {
+  return (
+    <div class="credit">
+    <p>
+      Coded by
+      <a href="https://www.shecodes.io/graduates/73033-chaimae-lamirine"
+        > Hashe Asiphe</a
+      > <a href="https://github.com/shemmee/React-Weather-App">All rights reserved</a> 2024
+    </p>
+  </div>
+  );
+}
