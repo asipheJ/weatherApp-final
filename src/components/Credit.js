8e83208432eba39bf1c5c5ee84eb644874c7f636
asiphej <hasheasiphe5@gmail.com>
2025-05-13 14:51:42 +0100

    Initial push of weather app

--- src/components/Credit.js
+++ src/components/Credit.js
@@ -4,10 +4,10 @@ export default function Credit() {
   return (
     <div class="credit">
     <p>
-      Coded by
+  
       <a href="https://weather-app-final-three.vercel.app/"
-        > Hashe Asiphe</a
-      > <a href="https://github.com/asipheJ/weatherApp-final">All rights reserved</a> 2024
+        > </a
+      > <a href="https://github.com/asipheJ/weatherApp-final"></a> 
     </p>
   </div>
   );
